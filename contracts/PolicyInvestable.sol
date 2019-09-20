a9c970ecb4827b1dfdbb6ce0bf079ca02c742b11
Preetam <raopreetam007@gmail.com>
2019-09-20 17:39:10 +0530

    Update PolicyInvestable.sol

--- contracts/PolicyInvestable.sol
+++ contracts/PolicyInvestable.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.10;
 contract PolicyInvestable {
   function invest() payable returns (bool success);
   function investd() payable returns (bool success);
+  function investc() payable returns (bool success);
 
   event Invested(uint value);
 }
